5113ce820d67ac268fc5589e3fbe4552be9c94db
Lokesh Shukla <125145184+LokeshShukla910@users.noreply.github.com>
2024-09-29 15:30:22 +0530

    Text Analyser.py

--- Text Analyser.py
+++ Text Analyser.py
@@ -1,15 +1,15 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-Analyser = SentimentIntensityAnalyzer()
-def analyser(text):
+Analyzer = SentimentIntensityAnalyzer()
+def analyzer(text):
     try:
-        sentiment = Analyser.polarity_scores(text)
-        if sentiment.compound >= 0.05:
+        sentiment = Analyzer.polarity_scores(text)
+        if sentiment("compound") >= 0.05:
             print("Positive")
-        elif sentiment.compound <= -0.05:
+        elif sentiment("compound") <= -0.05:
             print("Negetive")
         else:
             print("Neutral")
     except Exception as e:
         print(e)
 Text = input("Please enter a Text")
-analyser(Text)
+analyzer(Text)
